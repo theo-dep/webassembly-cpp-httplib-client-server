e9cd7782cc7084f50856bc24176c983f5202ac9d
Théo DEVAUCOUP <theo.devaucoup@protonmail.com>
2025-11-04 13:38:22 +0100

    add emscripten client compilation

--- server/main.cpp
+++ server/main.cpp
@@ -1,8 +1,19 @@
 #include <httplib.h>
 
-int main()
+#include <filesystem>
+
+int main(int, char** argv)
 {
+    const std::filesystem::path current_path{ std::filesystem::path(argv[0]).parent_path() };
     httplib::Server svr;
+
+    svr.Get("/", [&current_path](const httplib::Request&, httplib::Response& res) {
+        res.set_file_content((current_path / "client.html").string());
+    });
+
+    svr.Get("/client.(js|wasm)", [&current_path](const httplib::Request& req, httplib::Response& res) {
+        res.set_file_content((current_path / "client.").string() + req.matches[1].str());
+    });
 
     svr.Get("/hi", [](const httplib::Request&, httplib::Response& res) {
         res.set_content("Hello World!", "text/plain");
